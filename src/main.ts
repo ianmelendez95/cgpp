2d42b401e7b759ea20788cc9711d2eebf761f882
Ian Melendez <ian.melendez.95@gmail.com>
2026-02-25 21:07:24 -0600

    draw lines

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,25 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-camera.position.z = 5;
+const scene = new THREE.Scene();
 
-function animate(time: number) {
-    cube.rotation.x = time / 2000;
-    cube.rotation.y = time / 1000;
-    renderer.render(scene, camera);
-}
+const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+const points = [
+    new THREE.Vector3(-10, 0, 0),
+    new THREE.Vector3(0, 10, 0),
+    new THREE.Vector3(10, 0, 0),
+    new THREE.Vector3(-10, 0, 0),
+];
 
-renderer.setAnimationLoop(animate);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+
+renderer.render(scene, camera);
