66014a3b147141290c180ad890a2cb43366824d2
Ian Melendez <ian.melendez.95@gmail.com>
2026-02-25 21:16:33 -0600

    example helper render

--- src/mesh/exercises/simple1DExample.ts
+++ src/mesh/exercises/simple1DExample.ts
@@ -1,6 +1,18 @@
 import * as THREE from 'three';
+import {Vector3} from 'three';
 
 export default function simple1DExample() {
+    const points = [
+        new THREE.Vector3(-10, 0, 0),
+        new THREE.Vector3(0, 10, 0),
+        new THREE.Vector3(10, 0, 0),
+        new THREE.Vector3(-10, 0, 0),
+    ];
+
+    renderPoints(points);
+}
+
+function renderPoints(points: Vector3[]) {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
@@ -12,12 +24,6 @@ export default function simple1DExample() {
     const scene = new THREE.Scene();
 
     const material = new THREE.LineBasicMaterial({color: 0x0000ff});
-    const points = [
-        new THREE.Vector3(-10, 0, 0),
-        new THREE.Vector3(0, 10, 0),
-        new THREE.Vector3(10, 0, 0),
-        new THREE.Vector3(-10, 0, 0),
-    ];
 
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
     const line = new THREE.Line(geometry, material);
