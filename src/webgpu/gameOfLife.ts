04f2553ff6e1b3d52204ef62676b0ba9d3359453
Ian Melendez <ian.melendez.95@gmail.com>
2026-03-05 07:22:40 -0600

    render square

--- src/webgpu/gameOfLife.ts
+++ src/webgpu/gameOfLife.ts
@@ -21,7 +21,68 @@ export default async function gameOfLife() {
         format: canvasFormat
     });
 
+    const vertices = new Float32Array([
+        -0.8, -0.8,
+         0.8, -0.8,
+         0.8,  0.8,
+
+        -0.8, -0.8,
+         0.8,  0.8,
+        -0.8,  0.8
+    ]);
+
+    const vertexBuffer = device.createBuffer({
+        label: "Cell vertices",
+        size: vertices.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+    });
+
+    device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+    const vertexBufferLayout: GPUVertexBufferLayout = {
+        arrayStride: 8,
+        attributes: [{
+            format: "float32x2",
+            offset: 0,
+            shaderLocation: 0
+        }]
+    }
+
+    const cellShaderModule = device.createShaderModule({
+        label: "Cell shader",
+        code: /* wgsl */`
+            // Your shader code will go here
+            @vertex 
+            fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+                return vec4f(pos, 0, 1); // (x, y, z, w)
+            }
+
+            @fragment 
+            fn fragmentMain() -> @location(0) vec4f {
+                return vec4f(1, 0, 0, 1);
+            }
+        `
+    });
+
+    const cellPipeline = device.createRenderPipeline({
+        label: "Cell pipeline",
+        layout: "auto",
+        vertex: {
+            module: cellShaderModule,
+            entryPoint: "vertexMain",
+            buffers: [vertexBufferLayout]
+        },
+        fragment: {
+            module: cellShaderModule,
+            entryPoint: "fragmentMain",
+            targets: [{
+                format: canvasFormat
+            }]
+        }
+    });
+
     const encoder = device.createCommandEncoder();
+
     const pass = encoder.beginRenderPass({
         colorAttachments: [{
             view: context.getCurrentTexture().createView(),
@@ -30,6 +91,11 @@ export default async function gameOfLife() {
             clearValue: {r: 0, g: 0, b: 0.4, a: 1}
         }]
     });
+
+    pass.setPipeline(cellPipeline);
+    pass.setVertexBuffer(0, vertexBuffer);
+    pass.draw(vertices.length / 2);
+
     pass.end();
 
     device.queue.submit([encoder.finish()]);
