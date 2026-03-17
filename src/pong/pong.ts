e304b39abc62407cfc4a0527ba84ac4b4fb1941c
Ian Melendez <ian.melendez.95@gmail.com>
2026-03-16 20:15:50 -0500

    Pong class

--- src/pong/pong.ts
+++ src/pong/pong.ts
@@ -1,44 +1,73 @@
 import * as THREE from 'three';
 
 export default function pong() {
-    const canvas = document.getElementById('cgpp-canvas') as HTMLCanvasElement;
-    const {renderer, camera, scene} = initThree(canvas);
-
-    const paddleGeometry = new THREE.BoxGeometry(10, 100, 10);
-    const paddleMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-    const paddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
-    paddle.position.set(0, 0, 0);
-
-    scene.add(paddle);
-
-    renderer.render(scene, camera);
 }
 
-function initThree(canvas: HTMLCanvasElement) {
-    const renderer = new THREE.WebGLRenderer({
-        antialias: false, 
-        canvas
-    });
+class Pong { 
+    readonly scene: THREE.Scene;
+    readonly renderer: THREE.WebGLRenderer;
+    readonly camera: THREE.Camera;
 
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    readonly playerPaddle: Paddle;
 
-    const aspect = canvas.clientWidth / canvas.clientHeight;
-    const camera = new THREE.PerspectiveCamera(
-        50,
-        canvas.clientWidth / canvas.clientHeight,
-        1, 
-        1000);
-    camera.position.set(0, 0, 1000);
-    camera.lookAt(0, 0, 0);
+    constructor() {
+        const canvas = document.getElementById('cgpp-canvas') as HTMLCanvasElement;
+        const {renderer, camera, scene} = Pong.initThree(canvas);
 
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x2e2e2e);
+        this.renderer = renderer;
+        this.scene = scene;
+        this.camera = camera;
 
-    return {
-        renderer,
-        camera,
-        scene
+        this.playerPaddle = new Paddle();
+        this.playerPaddle.position.set(-300, 0, 0);
+
     }
+
+    start() {
+        this.scene.add(this.playerPaddle.object);
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    static initThree(canvas: HTMLCanvasElement) {
+        const renderer = new THREE.WebGLRenderer({
+            antialias: false, 
+            canvas
+        });
+
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+
+        const camera = new THREE.PerspectiveCamera(
+            50,
+            canvas.clientWidth / canvas.clientHeight,
+            1, 
+            1000);
+        camera.position.set(0, 0, 500);
+        camera.lookAt(0, 0, 0);
+
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color(0x2e2e2e);
+
+        return {
+            renderer,
+            camera,
+            scene
+        }
+    }
+
 }
 
-pong();
+class Paddle {
+    readonly object: THREE.Object3D;
+    readonly position: THREE.Vector3;
+
+    constructor () {
+        const paddleGeometry = new THREE.BoxGeometry(10, 100, 10);
+        const paddleMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+        this.object = new THREE.Mesh(paddleGeometry, paddleMaterial);
+        this.position = this.object.position;
+    }
+
+
+}
+
+new Pong().start();
