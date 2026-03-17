acbdd753df8d0f545e302874b06c7f93acc18d4f
Ian Melendez <ian.melendez.95@gmail.com>
2026-03-16 20:08:42 -0500

    my first paddle

--- src/pong/pong.ts
+++ src/pong/pong.ts
@@ -4,6 +4,13 @@ export default function pong() {
     const canvas = document.getElementById('cgpp-canvas') as HTMLCanvasElement;
     const {renderer, camera, scene} = initThree(canvas);
 
+    const paddleGeometry = new THREE.BoxGeometry(10, 100, 10);
+    const paddleMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
+    const paddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
+    paddle.position.set(0, 0, 0);
+
+    scene.add(paddle);
+
     renderer.render(scene, camera);
 }
 
@@ -16,16 +23,12 @@ function initThree(canvas: HTMLCanvasElement) {
     renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
     const aspect = canvas.clientWidth / canvas.clientHeight;
-    const desiredHalfHeight = 20;
-    const desiredHalfWidth = desiredHalfHeight * aspect;
-    const camera = new THREE.OrthographicCamera(
-        -desiredHalfWidth, 
-        desiredHalfWidth, 
-        desiredHalfHeight, 
-        -desiredHalfHeight, 
+    const camera = new THREE.PerspectiveCamera(
+        50,
+        canvas.clientWidth / canvas.clientHeight,
         1, 
         1000);
-    camera.position.set(0, 0, 5);
+    camera.position.set(0, 0, 1000);
     camera.lookAt(0, 0, 0);
 
     const scene = new THREE.Scene();
@@ -34,9 +37,7 @@ function initThree(canvas: HTMLCanvasElement) {
     return {
         renderer,
         camera,
-        scene,
-        viewWidth: desiredHalfWidth * 2,
-        viewHeight: desiredHalfHeight * 2
+        scene
     }
 }
 
